1f4e144dbb276484b6c7a8f42f76694ff9f1833e
Kaden Rotunda <102332339+kRotunda@users.noreply.github.com>
2023-02-06 09:22:30 -0700

    python def update

--- app.py
+++ app.py
@@ -7,23 +7,23 @@ def home():
     return render_template('home.html', base="base.html")
 
 @app.route("/Archeology")
-def home():
-    return render_template('home.html', base="base.html")
+def archeology():
+    return render_template('subHome.html', base="base.html", subject="Archeology")
 
 @app.route("/Biology")
-def home():
-    return render_template('home.html', base="base.html")
+def biology():
+    return render_template('subHome.html', base="base.html", subject="Biology")
 
 @app.route("/Geology")
-def home():
-    return render_template('home.html', base="base.html")
+def geology():
+    return render_template('subHome.html', base="base.html", subject="Geology")
 
 @app.route("/Paleontology")
-def home():
-    return render_template('home.html', base="base.html")
+def paleontology():
+    return render_template('subHome.html', base="base.html", subject="Paleontology")
 
 @app.route("/About_Us")
-def home():
+def aboutUs():
     return render_template('home.html', base="base.html")
 
 if __name__ == '__main__':
