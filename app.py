9c834ff8689d4979e87afddb751b891a4567c47e
Kaden Rotunda <102332339+kRotunda@users.noreply.github.com>
2023-02-06 09:14:45 -0700

    python routes

--- app.py
+++ app.py
@@ -6,5 +6,25 @@ app = Flask(__name__)
 def home():
     return render_template('home.html', base="base.html")
 
+@app.route("/Archeology")
+def home():
+    return render_template('home.html', base="base.html")
+
+@app.route("/Biology")
+def home():
+    return render_template('home.html', base="base.html")
+
+@app.route("/Geology")
+def home():
+    return render_template('home.html', base="base.html")
+
+@app.route("/Paleontology")
+def home():
+    return render_template('home.html', base="base.html")
+
+@app.route("/About_Us")
+def home():
+    return render_template('home.html', base="base.html")
+
 if __name__ == '__main__':
     app.run(debug=True)
